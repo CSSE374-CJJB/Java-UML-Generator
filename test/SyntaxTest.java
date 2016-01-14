e577d58aaf848e1535c7ac6f2fcf94774312d9b2
Jeremy Brinegar <brinegjr@rose-hulman.edu>
2016-01-14 13:16:21 -0500

    finished testing for current code

--- test/SyntaxTest.java
+++ test/SyntaxTest.java
@@ -1,10 +1,12 @@
 import static org.junit.Assert.*;
 import edu.rosehulman.cjjb.asm.Utils;
 import edu.rosehulman.cjjb.javaModel.JavaModel;
+import edu.rosehulman.cjjb.javaModel.Method;
 import edu.rosehulman.cjjb.javaModel.Relation;
 import edu.rosehulman.cjjb.javaModel.AbstractJavaElement;
 import edu.rosehulman.cjjb.javaModel.AbstractJavaStructure;
 import edu.rosehulman.cjjb.javaModel.Class;
+import edu.rosehulman.cjjb.javaModel.Field;
 import edu.rosehulman.cjjb.javaModel.Interface;
 import edu.rosehulman.cjjb.javaModel.modifier.*;
 import org.objectweb.asm.*;
@@ -185,6 +187,50 @@ public class SyntaxTest {
 		correct.add(new Relation(clazz, inter));
 		
 		assertEquals(correct, mod.getIncludedInterfaceRelations());
+	}
+	
+	@Test
+	public void testGetIncludedUsesRelations() {
+		Set<String> set = new HashSet<String>();
+		set.add("sampleClasses.Class1");
+		set.add("sampleClasses.Class2");
+		JavaModel mod = new JavaModel(set);
+		AbstractJavaStructure clazz = new Class("sampleClasses.Class1", (IAccessModifier)new PublicModifier(), new LinkedList<IModifier>(),
+				new LinkedList<AbstractJavaElement>(), new LinkedList<AbstractJavaStructure>(), (AbstractJavaStructure)new Class("Class2"));
+		Class clazz2 = new Class("sampleClasses.Class2");
+		Method meth = new Method("getClass2", (IAccessModifier) new PublicModifier(), new LinkedList<IModifier>(),
+				clazz2, new LinkedList<AbstractJavaStructure>());
+		clazz.addSubElement(meth);
+		
+		mod.putStructure("sampleClasses.Class1", clazz);
+		mod.putStructure("sampleClasses.Class2", clazz2);
+		
+		List<Relation> correct = new LinkedList<Relation>();
+		correct.add(new Relation(clazz, clazz2));
+		
+		assertEquals(correct, mod.getIncludedUsesRelations());
+	}
+	
+	@Test
+	public void testGetIncludedAssociationRelations() {
+		Set<String> set = new HashSet<String>();
+		set.add("sampleClasses.Class1");
+		set.add("sampleClasses.Class2");
+		JavaModel mod = new JavaModel(set);
+		AbstractJavaStructure clazz2 = new Class("sampleClasses.Class2", (IAccessModifier)new PublicModifier(), new LinkedList<IModifier>(),
+				new LinkedList<AbstractJavaElement>(), new LinkedList<AbstractJavaStructure>(), (AbstractJavaStructure)new Class("Class1"));
+		Class clazz = new Class("sampleClasses.Class1");
+		Field field = new Field("sampleClasses.Class1", (IAccessModifier) new PublicModifier(), new LinkedList<IModifier>(),
+				clazz);
+		clazz2.addSubElement(field);
+		
+		mod.putStructure("sampleClasses.Class1", clazz);
+		mod.putStructure("sampleClasses.Class2", clazz2);
+		
+		List<Relation> correct = new LinkedList<Relation>();
+		correct.add(new Relation(clazz2, clazz));
+		
+		assertEquals(correct, mod.getIncludedAssociationReltiations());
 	}
 	
 }
