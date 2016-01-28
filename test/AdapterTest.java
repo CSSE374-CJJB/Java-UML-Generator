e7dd5078ab62c13177a41a50722878ff898528f3
Jeremy Brinegar <brinegjr@rose-hulman.edu>
2016-01-28 13:03:10 -0500

    test stuff

--- test/AdapterTest.java
+++ test/AdapterTest.java
@@ -1,4 +1,36 @@
+import static org.junit.Assert.assertTrue;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.Test;
+
+import edu.rosehulman.cjjb.JavaModelClassVisitor;
 
 public class AdapterTest {
 
+	@Test
+	public void testCheckForAdapters() throws IOException {
+		Set<String> classes = new HashSet<String>();
+		classes.add("problem.client.App");
+		classes.add("problem.client.IteratorToEnumerationAdapter");
+		classes.add("problem.lib.LinearTransformer");
+		OutputStream out = new ByteArrayOutputStream();
+		JavaModelClassVisitor visitor = new JavaModelClassVisitor(classes, out);
+		visitor.buildUMLModel();
+		String output = out.toString();
+		
+		assertTrue(output.contains("problem.client.IteratorToEnumerationAdapter\\l\\<\\<adapter\\>\\>"));
+		assertTrue(output.contains("java.util.Enumeration\\l\\<\\<adaptee\\>\\>"));
+		assertTrue(output.contains("java.util.Iterator\\l\\<\\<target\\>\\>"));
+		assertTrue(output.contains("problem.client.IteratorToEnumerationAdapter -> java.util.Enumeration [label = \\<\\<adapts\\>\\>]"));
+
+		assertTrue(!output.contains("problem.lib.LinearTransformer\\l\\<\\<decorator\\>\\>"));
+		assertTrue(!output.contains("problem.client.App\\l\\<\\<decorator\\>\\>"));
+	}
+	
 }
+
--- test/DecoratorTest.java
+++ test/DecoratorTest.java
@@ -1,4 +1,48 @@
+import static org.junit.Assert.*;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.Test;
+
+import edu.rosehulman.cjjb.JavaModelClassVisitor;
 
 public class DecoratorTest {
 
+	@Test
+	public void testCheckForDecorators() throws IOException {
+		Set<String> classes = new HashSet<String>();
+		classes.add("headfirst.decorator.io.InputTest");
+		classes.add("headfirst.decorator.io.LowerCaseInputStream");
+		classes.add("headfirst.decorator.starbuzz.Beverage");
+		classes.add("headfirst.decorator.starbuzz.CondimentDecorator");
+		classes.add("headfirst.decorator.starbuzz.DarkRoast");
+		classes.add("headfirst.decorator.starbuzz.Decaf");
+		classes.add("headfirst.decorator.starbuzz.Espresso");
+		classes.add("headfirst.decorator.starbuzz.HouseBlend");
+		classes.add("headfirst.decorator.starbuzz.Milk");
+		classes.add("headfirst.decorator.starbuzz.Mocha");
+		classes.add("headfirst.decorator.starbuzz.Soy");
+		classes.add("headfirst.decorator.starbuzz.StarbuzzCoffee");
+		classes.add("headfirst.decorator.starbuzz.Whip");
+		OutputStream out = new ByteArrayOutputStream();
+		JavaModelClassVisitor visitor = new JavaModelClassVisitor(classes, out);
+		visitor.buildUMLModel();
+		String output = out.toString();
+		
+		assertTrue(output.contains("headfirst.decorator.starbuzz.Beverage\\l\\<\\<component\\>\\>"));
+		assertTrue(output.contains("headfirst.decorator.starbuzz.Milk\\l\\<\\<decorator\\>\\>"));
+		assertTrue(output.contains("headfirst.decorator.starbuzz.Mocha\\l\\<\\<decorator\\>\\>"));
+		assertTrue(output.contains("headfirst.decorator.starbuzz.Soy\\l\\<\\<decorator\\>\\>"));
+		assertTrue(output.contains("headfirst.decorator.starbuzz.Whip\\l\\<\\<decorator\\>\\>"));
+		assertTrue(output.contains("headfirst.decorator.starbuzz.CondimentDecorator\\l\\<\\<decorator\\>\\>"));
+		assertTrue(output.contains("headfirst.decorator.starbuzz.CondimentDecorator -> headfirst.decorator.starbuzz.Beverage [label = \\<\\<decorates\\>\\>]"));
+
+		assertTrue(!output.contains("headfirst.decorator.starbuzz.StarbuzzCoffee\\l\\<\\<decorator\\>\\>"));
+		assertTrue(!output.contains("headfirst.decorator.io.InputTest\\l\\<\\<decorator\\>\\>"));
+	}
+	
 }
